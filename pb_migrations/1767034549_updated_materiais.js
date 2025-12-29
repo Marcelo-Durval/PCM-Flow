2bee1ab2ff1d92a7ba50f1eeb61151b05e6f4388
marcelo-durval <marceloalexandre9982@gmail.com>
2025-12-29 16:32:22 -0300

    Adicionado tela de aprovação e melhorias para o uso mobile

---
+++ pb_migrations/1767034549_updated_materiais.js
@@ -0,0 +1,57 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("iayyrlsf0zb5ztt")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "traji2k4",
+    "name": "status_compra",
+    "type": "select",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "values": [
+        "Em cotação",
+        "Aguardando aprovação",
+        "Aguardando pagamento",
+        "Aguardando entrega",
+        "Entregue",
+        "Cancelado",
+        "Aprovado"
+      ]
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("iayyrlsf0zb5ztt")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "traji2k4",
+    "name": "status_compra",
+    "type": "select",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "values": [
+        "Em cotação",
+        "Aguardando aprovação",
+        "Aguardando pagamento",
+        "Aguardando entrega",
+        "Entregue",
+        "Cancelado"
+      ]
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
---
+++ pb_migrations/1767034686_updated_users.js
@@ -0,0 +1,51 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "pxbbme34",
+    "name": "perfil",
+    "type": "select",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "values": [
+        "admin",
+        "operador",
+        "visualizador",
+        "aprovador"
+      ]
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "pxbbme34",
+    "name": "perfil",
+    "type": "select",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "values": [
+        "admin",
+        "operador",
+        "visualizador"
+      ]
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
